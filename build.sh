51f8c26d9f2b002a6a8af9fc562b92ff788f1efc
vincentlemoinefr <vincent.lemoine.fr@gmail.com>
2024-08-03 12:16:24 +0200

    Automatic commit for 1.0.0

--- build.sh
+++ build.sh
@@ -9,6 +9,11 @@ git add .
 
 git commit -m "Automatic commit for $version"
 
+if [ -z "$(docker images -q $package_name:$version-$commit_hash 2> /dev/null)" ]; then
+  echo "You have not made any changes, rebuild is unecessary"
+  exit(0)
+fi
+
 docker build . --check
 
 docker build . \
