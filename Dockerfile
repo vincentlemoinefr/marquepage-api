310c4aa73f65d73349b86d12c7df66d1f361b039
vincentlemoinefr <vincent.lemoine.fr@gmail.com>
2024-08-03 12:07:22 +0200

    Automatic commit for 1.0.0

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
+# syntax=docker/dockerfile:1
+# check=error=true
+
 FROM node:lts-alpine3.20
 
+ARG BUILD_DATE
+ARG BUILD_VERSION
+LABEL BUILD_DATE=$BUILD_DATE
+LABEL BUILD_VERSION=$BUILD_VERSION
+
 RUN mkdir -p /marquepage
-
 WORKDIR /marquepage
-
 COPY . .
 
 RUN npm ci
--- build.sh
+++ build.sh
@@ -1,21 +1,38 @@
-name=$(npm pkg get name)
-version=$(npm pkg get version --workspaces=false)
-short_date=$(date -u +'%y%m%d-%H%M')
+package_name=$(npm pkg get name | tr -d '"')
+version=$(npm pkg get version --workspaces=false | tr -d '"')
 full_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+commit_hash=$(git rev-parse --short HEAD)
 
-echo ${name//\"/}:${version//\"/}-$full_date
+echo $package_name:$version-$commit_hash
 
-#docker build marquepage-api:$version .
+git add .
+
+git commit -m "Automatic commit for $version"
+
+docker build . --check
+
+docker build . \
+  --build-arg BUILD_DATE=$full_date \
+  --build-arg BUILD_DATE=$version \
+  -t $package_name:$version-$commit_hash
+
+
+
 
 # Blue/Green Trunk dev cycle :
-# Dev pull blue(trunk)
-# Make a feature branch
-# Can push whatever code on that feature branch
-# Local testing validate the code
-# only if the testing are ok can you merge on trunk and publish to image repo
-
-# Build a test image
-# Do the testing
-# Then build a clean production ready image
-
-
+# 1/ dev something
+# 1.1/ dev use command "build"
+# 2/ git add / git commit [no push] (automatic)
+# 3/ build command launch :
+#   - get package name
+#   - get short commit hash
+#   - get version
+# 3.1/ docker build command with image name : "name:version-githash"
+# 4/ build success
+#   - if fail : last commit reverted, image deleted
+#   - if success : next
+# 5/ We need infrastructure : local virtualbox / vmware (vagrant)
+# 5.1/ We don't want to rebuild the docker running VM everytime
+# 6/ isolated testing on that image (no external api or db)
+#   - if fail : last commit reverted, container deleted, image deleted
+#   - if success : increment version, git add, git commit new version, git puh allowed
