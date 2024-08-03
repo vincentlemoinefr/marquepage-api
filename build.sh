6b41f39b4561c487fb21ef1400d51e1157a58395
vincentlemoinefr <vincent.lemoine.fr@gmail.com>
2024-08-03 12:24:25 +0200

    Automatic commit for 1.0.0

--- build.sh
+++ build.sh
@@ -4,30 +4,28 @@ full_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 commit_hash=$(git rev-parse --short HEAD)
 
 git add .
-if [ $? -ne 0 ]; then exit 0;fi
+if [ $? -ne 0 ]; then exit 0; fi
 
 git commit -m "Automatic commit for $version"
-if [ $? -ne 0 ]; then; exit 0; fi
+if [ $? -ne 0 ]; then exit 0; fi
 
 echo "Building image : $package_name:$version-$commit_hash"
-if [ $? -ne 0 ]; then; exit 0; fi
+if [ $? -ne 0 ]; then exit 0; fi
 
 docker build . --check
-if [ $? -ne 0 ]; then
-  exit 0
-fi
+if [ $? -ne 0 ]; then exit 0; fi
 
 docker build . \
   --build-arg BUILD_DATE=$full_date \
   --build-arg BUILD_DATE=$version \
   -t $package_name:$version-$commit_hash
-if [ $? -ne 0 ]; then; exit 0; fi
+if [ $? -ne 0 ]; then exit 0; fi
 
 docker scout quickview $package_name:$version-$commit_hash
-if [ $? -ne 0 ]; then; exit 0; fi
+if [ $? -ne 0 ]; then exit 0; fi
 
 docker scout recommendations $package_name:$version-$commit_hash
-if [ $? -ne 0 ]; then; exit 0; fi
+if [ $? -ne 0 ]; then exit 0; fi
 
 
 
